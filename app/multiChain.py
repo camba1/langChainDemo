d3d8f7f3a29ccd3a57cbb5e2b7fa9f9e1b65c6d7
Juan Peredo <jperedo@bolbeck.com>
2024-03-15 19:35:16 -0500

    cleaned up routable chain

--- app/multiChain.py
+++ app/multiChain.py
@@ -5,6 +5,8 @@ from langchain_core.prompts import ChatPromptTemplate
 # PromptTemplate allow the creation of prompt from a simple string
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableSerializable
+from langchain.pydantic_v1 import BaseModel
 
 
 # Model constants
@@ -15,7 +17,6 @@ PRIMARY_MODEL_NAME = "gpt-fake"
 STAR_TREK_MODEL_NAME = "gpt-3.5-turbo"
 STAR_WARS_MODEL_NAME = "gpt-4"
 MODEL_TEMPERATURE = 0
-
 
 
 def chain_with_fallback():
@@ -56,66 +57,76 @@ def chain_with_fallback():
 # print(result)
 
 
+def create_chain(prompt_text:str, model_temperature, model_name) -> RunnableSerializable:
+    """
+    Create a new chain object
+    :param prompt_text: Text that will e used as part of the prompt sent to llm
+    :param model_temperature: Zero to 1 where zero is factual and 1 imaginative
+    :param model_name: Name of the model to use
+    :return: New chain object
+    """
+    model = ChatOpenAI(temperature=model_temperature, model=model_name)
+    classify_prompt = PromptTemplate.from_template(prompt_text)
+    classification_chain = classify_prompt | model | StrOutputParser()
+    return classification_chain
+
+
+# Class used to indicate the expected input format since the Agent runnable does not expose by default
+class QuestionInput(BaseModel):
+    question: str
+
+
 def routable_chain():
 
-    model = ChatOpenAI(temperature=MODEL_TEMPERATURE, model=MODEL_NAME)
-
+    # Initial chain
     classify_prompt_text = """You are an expert in science fiction movies. In particular you are well verse in the
     movies from 'Star Trek' and 'Star Wars'. Classify if the user question below refers to 'Star Trek', 'Star Wars' 
     or 'other'.
      Keep you answer to a maximum of two words.
-     
+
      <question>
      {question}
      </question>
-     
+
      Classification:"""
+    classification_chain = create_chain(classify_prompt_text, MODEL_TEMPERATURE, MODEL_NAME)
 
-    classify_prompt = PromptTemplate.from_template(classify_prompt_text)
-
-    classification_chain = classify_prompt | model | StrOutputParser()
-
-    startrek_model = ChatOpenAI(temperature=MODEL_TEMPERATURE, model=STAR_TREK_MODEL_NAME)
-
+    # chain 1
     startrek_prompt_text = """You are an expert in Star Trek movies.
-        Answer all the questions with " Spock once mentioned that"
+        Answer all the questions starting with " Spock once mentioned that"
         
         Question:{question} 
         Answer:"""
+    startrek_chain = create_chain(startrek_prompt_text, MODEL_TEMPERATURE, STAR_TREK_MODEL_NAME)
 
-    startrek_prompt = PromptTemplate.from_template(startrek_prompt_text)
-
-    startrek_chain = startrek_prompt | startrek_model | StrOutputParser()
-
-    starwars_model = ChatOpenAI(temperature=MODEL_TEMPERATURE, model=STAR_WARS_MODEL_NAME)
-
+    # Chain 2
     starwars_prompt_text = """You are an expert in Star Wars movies.
-        Answer all the questions with "As Leia always says"
+        Answer all the questions starting with "As Leia always says"
         
         Question:{question} 
         Answer:"""
+    starwars_chain = create_chain(starwars_prompt_text, MODEL_TEMPERATURE, STAR_WARS_MODEL_NAME)
 
-    starwars_prompt = PromptTemplate.from_template(starwars_prompt_text)
-
-    starwars_chain = starwars_prompt | starwars_model | StrOutputParser()
-
-    general_prompt_text = """You are an expert in Star Wars movies.
-        Answer all the questions with "As Leia always says"
+    # chain 3
+    general_prompt_text = """You are helpful assistant.
+        Answer all the questions starting with "I do believe"
 
         Question:{question} 
         Answer:"""
+    general_chain = create_chain(general_prompt_text, MODEL_TEMPERATURE, MODEL_NAME)
 
-    general_prompt = PromptTemplate.from_template(general_prompt_text)
-
-    general_chain = general_prompt | model | StrOutputParser()
-
+    # Final Chain (Composite of all other chains)
     route = lambda x : startrek_chain if "star trek" in x["topic"].lower() \
         else (starwars_chain if "star wars" in x["topic"].lower() else general_chain)
 
-    complete_chain = {"topic": classification_chain, "question": lambda x : x["question"]} | RunnableLambda(route)
+    final_chain = {"topic": classification_chain, "question": lambda x : x["question"]} | RunnableLambda(route)
 
-    return complete_chain
+    return final_chain.with_types(input_type=QuestionInput)
 
 
-result = routable_chain().invoke({"question": "Who is Hans Solo?"})
-print(result)
+# result = routable_chain().invoke({"question": "Who is Hans Solo?"})
+# print(result)
+# result = routable_chain().invoke({"question": "Who is captain Kirk?"})
+# print(result)
+# result = routable_chain().invoke({"question": "Who is Madonna?"})
+# print(result)
