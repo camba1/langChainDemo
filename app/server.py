c035edf58256419b0127c6b8e7be4a70e5723547
Juan Peredo <jperedo@bolbeck.com>
2024-03-18 22:47:12 -0500

    cleanup

--- app/server.py
+++ app/server.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
-from pirate_speak.chain import chain as pirateChain
 from .myChain import chain as simple_chain
+from .openRouter import chain as openrouter_chain
 from .rag import build_chain
 from .agent import build_agent
 from .multiChain import chain_with_fallback, routable_chain
-from .openRouter import chain as openrouter_chain
+from pirate_speak.chain import chain as pirateChain
 app = FastAPI()
 
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
-
 
 # Edit this to add the chain you want to add
 add_routes(app, simple_chain, path="/simple")
@@ -22,7 +21,6 @@ add_routes(app, build_chain(), path="/rag")
 add_routes(app, build_agent(), path="/agent")
 add_routes(app, chain_with_fallback(), path="/multichain")
 add_routes(app, routable_chain(), path="/route")
-
 
 # Showing the endpoint and feedback require setting up langsmith access
 add_routes(app,
