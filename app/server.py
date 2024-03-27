0c7fad30763387030a1c25f40b8ff5eb50e2dde7
jperedo <peredo01@gmail.com>
2024-03-27 17:39:25 -0500

    Added simple authentication code example

--- app/server.py
+++ app/server.py
@@ -7,8 +7,30 @@ from .rag import build_chain
 from .agent import build_agent
 from .multiChain import chain_with_fallback, routable_chain
 from pirate_speak.chain import chain as pirateChain
-app = FastAPI()
 
+# -------- Simple Auth ------------
+# Uncomment this section to enable simple auth using the contents of a 'x-token' header
+# Because we apply the security att he FastApi creation level, this applies to all paths
+# created with add_routes. Alternatively this could ba added at api path level instead.
+from typing_extensions import Annotated
+from fastapi import Depends, Header, HTTPException
+
+
+# async def verify_token(x_token: Annotated[str, Header()]) -> None:
+#     """Verify the token is valid."""
+#     # Replace this with your actual authentication logic
+#     print(x_token)
+#     if x_token != "secret-token":
+#         raise HTTPException(status_code=400, detail="X-Token header invalid")
+#
+# app = FastAPI(dependencies=[Depends(verify_token)])
+
+# --------End Simple Auth ------------
+
+# -------- No Auth --------------------
+# Comment this line if using the auth code above
+app = FastAPI()
+# -------- End No Auth ----------------
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -21,6 +43,7 @@ add_routes(app, build_chain(), path="/rag")
 add_routes(app, build_agent(), path="/agent")
 add_routes(app, chain_with_fallback(), path="/multichain")
 add_routes(app, routable_chain(), path="/route")
+
 
 # Showing the endpoint and feedback require setting up langsmith access
 add_routes(app,
